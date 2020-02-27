b364a846e293c8a25a935d836d09cd755636aaba
vedforgit <vedant2000raval@gmail.com>
2020-02-28 02:20:57 +0530

    something on card

--- src/java/dd.java
+++ src/java/dd.java
@@ -2,7 +2,17 @@
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
 import java.io.*;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.apache.pdfbox.pdmodel.interactive.form.PDField;
+
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import mail.ResizeImageExample;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -20,12 +30,17 @@ public class dd
         System.out.println("getImgpage-start"); 
                     PDDocument pDDocument = PDDocument.load(new File("C:/dependency/healthcard.pdf"));
                     PDAcroForm pDAcroForm = pDDocument.getDocumentCatalog().getAcroForm();
+//                    PDPage page = (PDPage)pDDocument.getDocumentCatalog().getAllPages().get(0);
+//                    PDPageContentStream contentStream = new PDPageContentStream(pDDocument, page ,true, true);
+//                    contentStream.drawImage( ximage, 425, 675 );
 //                    pDAcroForm.
 //                    PDImageXObject imgx = PDImageXObject.createFromByteArray(pDDocument, byteArray,".jpg");
 //                    PDPage paged = pDDocument.getPage(0);
 //                    PDPageContentStream contents = new PDPageContentStream(pDDocument,paged);
 //                    contents.drawImage(imgx,165,250);
 //                    pDDocument.getDocument().;
+
+
                     PDField field = pDAcroForm.getField("name");
                     field.setValue("Vedant Raval");
                     
@@ -69,9 +84,38 @@ public class dd
                     File file = new File("C:/dependency/shubham.pdf");
                     
                     pDDocument.save(file);
-                    System.out.println("getImgpage-end");
-//                    
+                    
+                    PDDocument doc = null;
+        try {
+            
+            File file1 = new File("C:/dependency/shubham.pdf");
+            doc = PDDocument.load(file1);
+            // Choose IMAGE File
+//            ResizeImageExample rie = new ResizeImageExample();
+            File img = new File("C:/dependency/rsz_cps_0006.jpg");
+//            File input = new File("C:/Users/vedant/Downloads/CPS_0006.jpg");
+            BufferedImage image = ImageIO.read(img);
+            
+            image = ResizeImageExample.resize(image, 0, 0);
+            
+            
+            PDImageXObject pdImage = PDImageXObject.createFromFile("C:/dependency/rsz_cps_0006.jpg", doc);
+//            PDImageXObject pdImage = ;
+//            pdImage.setHeight(14);
+//            pdImage.setWidth(50);
+            PDPage page = doc.getPage(0);
+            PDPageContentStream contentStream = new PDPageContentStream(doc, page);
+            // Set Position of image on PDF
+            contentStream.drawImage(pdImage,30,30);
+            contentStream.close();
+            doc.save("C:/dependency/shubham1.pdf");
+            doc.close();
+                    
         
     }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     
+}
 }
